afd9401b3c4820bbb2daf96fed28a4fef3afb737
Soumay kanti Das <anikds2859@outlook.com>
2021-06-03 17:51:32 +0600

    Add project files.

---
+++ Overloading+/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Overloading_
+{
+    class TestComplex
+    {
+        static void Main()
+        {
+            Complex num1 = new Complex(2, 3);
+            Complex num2 = new Complex(3, 4);
+            //add two complex objects through the overloading plus operator:
+            Complex sum = num1 + num2;
+            // print the numbers and the sum using the overriden Tostring method:
+            Console.WriteLine("Frist complex number: {0},num1");
+            Console.WriteLine("The sum of the two numbers:{0}", sum);
+        }
+    }
+}
---
+++ Overloading+/struct Complex.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Overloading_
+{
+    class struct Complex
+    {
+        public int real;
+        public int imaginary;
+
+    }
+    public Complex(int real,int imaginary) //constructor
+    {
+        this.real = real;
+        this.imaginary = imaginary;
+
+    }
+    //Declare which operator to overload (+),
+    //the types that can be added (two Complex object)
+    //and the return type (complex):
+    public static Complex operator +(Complex c1,Complex c2)
+    {
+        return new Complex(c1.real + c2.real, c1.imaginary + c2.imaginary);
+    }
+    //Override the toString() method to display a complex number in the traditional format:
+    public override string toSrring()
+    {
+        return (System.String.Format("{0}+{1}i", real.imaginary));
+
+    }
+
+}
